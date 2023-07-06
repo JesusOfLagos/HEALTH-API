1d9d78c3d86c04fe7556376023152c968df05bbf
Jesus Akanle <jesuswrites20043@gmail.com>
2023-07-06 12:37:34 +0000

    Finalized the Doctor Model

--- Server/Models/Doctors.js
+++ Server/Models/Doctors.js
@@ -23,8 +23,77 @@ const DoctorSchema = new Schema({
         required: true
     },
 
+    location: {
+        type: String,
+        required: true
+    },
+
+    speciality: {
+        type: String,
+        required: true
+    },
+
+    rating: {
+        type: Number,
+        required: false
+    },
+
+    reviews: {
+        type: [],
+        required: false
+    },
+
+    availability: {
+        type: String,
+        required: false
+    },
+
+    insuranceAccepted: {
+        type: [],
+        required: false
+    },
+
+    experience: {
+        type: String,
+        required: true
+    },
+
+    gender: {
+        type: String,
+        required: true
+    },
+
+    age: {
+        type: Number,
+        required: true
+    },
+
+    contact: {
+        type: String,
+        required: true
+    },
+
+    affiliation: {
+        type: String,
+        required: false
+    },
+
+    languageSpoken: {
+        type: [],
+        required: true
+    },
+
+    image: {
+        type: String,
+        required: false
+    },
+
     verified: {
         type: Boolean,
         required: false
     }
 })
+
+
+
+
