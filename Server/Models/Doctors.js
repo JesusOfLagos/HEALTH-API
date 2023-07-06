786e7a05cf3540834d99e299553bfdaa29378742
Jesus Akanle <jesuswrites20043@gmail.com>
2023-07-06 12:18:30 +0000

    Started the modlels

---
+++ Server/Models/Doctors.js
@@ -0,0 +1,20 @@
+const Mongoose = require("mongoose");
+const Schema = Mongoose.Schema
+
+const DoctorSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+
+    lastName: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }
+})
