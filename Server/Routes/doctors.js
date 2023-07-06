6da6802d66cf5d7d27a2e305ee13973f3066b0a7
Jesus Akanle <jesuswrites20043@gmail.com>
2023-07-06 13:59:07 +0000

    Started the doctors route

---
+++ Server/Routes/doctors.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const router = express.Router()
+
+const mongoose = require("mongoose")
+
+const JWT = require("jsonwebtoken")
+
+
+
+
+
+
+
+
+
+
+
+
+router.post("users/auth", (req, res) => {
+
+})
+
+
+
+router.post("users/auth/login", (req, res) => {
+
+})
