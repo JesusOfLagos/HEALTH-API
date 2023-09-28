ea5178d6f7ed4bd2469c4639917987634386fea7
Jesus Akanle <jesuswrites20043@gmail.com>
2023-09-28 23:58:00 +0100

    test after script issue?

--- Server/index.ts
+++ Server/index.ts
@@ -1,52 +1,47 @@
+console.log("Starting...")
+import express, { Express } from "express"
+import mongoose, { ConnectOptions } from "mongoose"
+import morgan from "morgan"
+import cors from "cors"
+import dotenv from "dotenv"
+import GlobalRouter from "./Routes/index.route"
+import UserRouter from "./Routes/User/user.route"
 
-import * as express from "express";
-import * as mongoose from "mongoose";
-import * as cors from "cors";
-import * as morgan from "morgan";
-import * as dotenv from "dotenv";
-// import GlobalRouter from "./Routes/index.route";
-// import UserRouter from "./Routes/User/user.route";
-dotenv.config();
+// dotenv.config()
 
-// app
-const app: express.Application = express();
+export const app: Express = express()
 
-// middleware
-app.use(morgan('combined')); // Use Morgan for logging
-app.use(express.json());
-app.use(cors({ origin: true, credentials: true }));
-// app.use('/api/v1', GlobalRouter)
-// app.use('/api/v1/users', UserRouter)
+app.use(morgan('combined'))
+app.use(express.json())
+app.use(cors({ origin: true, credentials: true }))
+app.use('/api/v1', GlobalRouter)
+app.use('/api/v1/users', UserRouter)
 app.use((req: any, res: any) => {
     res.status(404).json({ message: 'You arrived at Healthish, But with the wrong request!😒' });
-});
+}
+);
 
-//
-
-//port
-const port = process.env.PORT || 8090;
-console.log(process.env.MONGO_URI as string);
-
-//db
+const port = process.env.PORT || 8090
+console.log(process.env.MONGO_URI as string)
 
 async function connectDB() {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI as string, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-        } as mongoose.ConnectOptions)
+        } as ConnectOptions)
         console.log(`MongoDB Connected: ${conn.connection.host}`)
     } catch (error: any) {
         console.log({ message: "Cannot connect to Database"},`Error: ${error.message}`)
     }
 }
 
-//listener
 app.listen(port, () => { 
     console.log(`Server Is Running on Port ${port}`) 
-});
+}
+
+);
+
 connectDB();
-
-
 
 
